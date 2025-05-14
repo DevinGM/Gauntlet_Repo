2b0676d1cc67b0c7598928c467eef05575349635
Devin Monaghan <dgmonaghan7@gmail.com>
2025-05-14 08:11:03 -0700

    DM Commit 17
    - Readded ReadMe

---
+++ Source/Gauntlet_Unreal/ReadMe.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+/// <summary>
+/// Devin G Monaghan 
+/// 5/14/2025
+/// Holds ReadMe
+/// </summary>
+
+#include "ReadMe.h"
+
+ReadMe::ReadMe()
+{
+}
+
+ReadMe::~ReadMe()
+{
+}
--- Source/Gauntlet_Unreal/ReadMe.h
+++ Source/Gauntlet_Unreal/ReadMe.h
@@ -6,22 +6,27 @@
 /// Holds ReadMe
 /// </summary>
 
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAUNTLET_UNREAL_API ReadMe
+{
+public:
+	ReadMe();
+	~ReadMe();
+};
+
+
+
 /*
-	Devin worked mostly in C++ and made the ParentPlayer, BulletParent, CustomGameModeBase, 
+	Devin worked mostly in C++ and made the ParentPlayer, BulletParent, CustomGameModeBase,
 	DoorParent, HUD_Parent, ItemParent, MultiPlayerCamera, and PlayerInputManager C++ classes,
 	and the BP_Players, BP_PlayerBullets, BP_Items, BP_Door, BP_LevelEnd, and BP_HUD Blueprint classes
 	Devin also built the level and populated it with items
-
-
-
-
-
-
-
-
-
-
-
 
 
 
